02c5f5102a782e14a7a82fbac3fd51514b7461a9
Ravinder219 <reddyravi219@gmail.com>
2025-07-02 20:10:32 +0530

    Update4 demo.sh

--- demo.sh
+++ demo.sh
@@ -1,22 +1,2 @@
-o#!/bin/bash
-
-echo "📋 Jenkins Build Script Starting"
-ech "Node: $(hostname)"
-echo "Build Triggered by: $USER"
-echo "Time: $(date)"
-echo ""
-
-echo "✅ Checking disk space:"
-df -h /
-
-echo "gelddl"
-echo "⚙️ Simulating build task..."
-mkdir -p build_output
-echo "Build completed at $(date)" > build_output/result.txt
-
-echo "jhkh
-echo "📦 Archiving build artifact:"
-tar -czf build_output.tar.gz build_output/
-
-echo ""
-echo "🏁 Jenkins Build Script Completed"
+ech "teggst"
+echo "">test.tx
